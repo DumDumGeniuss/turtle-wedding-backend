d45cda111cd3b5b3dae5d5f611dd40ebe629103f
DumDumGeniuss <messi_yang@syntrontech.com>
2016-09-04 19:40:17 +0800

    Remove mmmagic, fuck

--- src/lib/files.js
+++ src/lib/files.js
@@ -1,28 +1,20 @@
 import path from "path";
 import fs from "fs";
 import Promise from "bluebird";
-import mmm from 'mmmagic';
 import config from '../config/config';
 
-const Magic = mmm.Magic;
 const PHOTOS_PATH = path.join(process.cwd(), config.photoPath);
 
 module.exports = {
     getPhotoFile: (filename) => {
         return new Promise( (resolve, reject) => {
             let filePath = path.join(PHOTOS_PATH, filename);
-            let magic = new Magic(mmm.MAGIC_MIME_TYPE);
 
             try {
                 fs.accessSync(filePath, fs.F_OK);
             } catch(e) {
                 reject(e);
             }
-
-            magic.detectFile(filePath, function(err, result) {
-                if (err) { reject(err) };
-                resolve({ filePath: filePath, fileMime:result });
-            });
         });
     },
     deletePhotoFile: (filename) => {
